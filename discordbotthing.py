0659e2bc048c26d173bafedf349e335200d24611
Isak Erikson Winbäck <92306017+indashades@users.noreply.github.com>
2026-05-17 12:19:08 +0200

    Add files via upload

--- discordbotthing.py
+++ discordbotthing.py
@@ -21,9 +21,10 @@ def economy_loop():
 
     while True:
         now = datetime.now()
-
+        print("uhhh..."+now.hour)
         if now.hour in [0, 4, 8, 12, 16, 20]:
             slot = now.hour
+            print("now in slot: "+slot)
 
             if slot != last_run:
                 countries = []
